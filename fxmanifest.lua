68cf9163a05b12342fc31b1e68dabb0d454a3045
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-08-18 23:04:41 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 
 fx_version "cerulean"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-games {"rdr3"}
+game "rdr3"
+lua54 'yes'
 
 files {
     'not.js',
@@ -15,7 +16,6 @@ client_scripts {
     'client.lua',
     'not.js'
 }
-
 
 server_scripts {
     'server.lua',
