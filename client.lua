ff853a70062445dd6c38e4b865db22f4b8de52cc
Outsider <87246847+outsider31000@users.noreply.github.com>
2026-03-23 17:57:24 +0000

    refactor client and server
    added anti exploit features
    added tool per grave
    added chance per item instead of just getting one item
    added amount per item you can use math.random to generate random amount
    added config for cooldown per robbery so its not until restart
    remove index ids arrays already have them built in
    added new table to avoid duplicated code
    optimized the script

--- client.lua
+++ client.lua
@@ -1,88 +1,105 @@
 ---@diagnostic disable: undefined-global
 local TEXTS = Config.Texts
 local TEXTURES = Config.Textures
-local pcoords = nil
-local isdead = nil
 local praying = false
 local digging = true
 local shovelObject = nil
 local PromptKey
 local PromptKey2
 local PromptGroup = GetRandomIntInRange(0, 0xffffff)
-local Town
 local prompts = {}
 
 function TableNum(tbl)
     local c = 0
-    for i, v in pairs(tbl) do
+    for _, _ in pairs(tbl) do
         c = c + 1
     end
     return c
 end
 
+local districts <const> = {
+    [459833523] = "Valentine",
+    [1053078005] = "Blackwater",
+    [427683330] = "Strawberry",
+    [-765540529] = "Saint Denis",
+    [7359335] = "Annesburg",
+    [-744494798] = "Armadillo",
+    [2046780049] = "Rhodes",
+    [2126321341] = "Vanhorn",
+    [-1524959147] = "Tumbleweed",
+}
+
+local function getTown(pcoords)
+    local district = Citizen.InvokeNative(0x43AD8FC02B429D33, pcoords.x, pcoords.y, pcoords.z, 1)
+    return districts[district]
+end
+
 function LoadPrompts()
     local str = TEXTS.Prompt1
-    PromptKey = PromptRegisterBegin()
-    PromptSetControlAction(PromptKey, Config.Prompts.Prompt1)
-    str = CreateVarString(10, 'LITERAL_STRING', str)
-    PromptSetText(PromptKey, str)
-    PromptSetEnabled(PromptKey, 1)
-    PromptSetVisible(PromptKey, 1)
-    PromptSetStandardMode(PromptKey, 1)
-    PromptSetGroup(PromptKey, PromptGroup)
-    Citizen.InvokeNative(0xC5F428EE08FA7F2C, PromptKey, true)
-    PromptRegisterEnd(PromptKey)
+    PromptKey = UiPromptRegisterBegin()
+    UiPromptSetControlAction(PromptKey, Config.Prompts.Prompt1)
+    str = VarString(10, 'LITERAL_STRING', str)
+    UiPromptSetText(PromptKey, str)
+    UiPromptSetEnabled(PromptKey, true)
+    UiPromptSetVisible(PromptKey, true)
+    UiPromptSetStandardMode(PromptKey, true)
+    UiPromptSetGroup(PromptKey, PromptGroup, 0)
+    UiPromptRegisterEnd(PromptKey)
     prompts[#prompts + 1] = PromptKey
 
-    local str = TEXTS.Prompt2
-    PromptKey2 = PromptRegisterBegin()
-    PromptSetControlAction(PromptKey2, Config.Prompts.Prompt2)
-    str = CreateVarString(10, 'LITERAL_STRING', str)
-    PromptSetText(PromptKey2, str)
-    PromptSetEnabled(PromptKey2, 1)
-    PromptSetVisible(PromptKey2, 1)
-    PromptSetStandardMode(PromptKey2, 1)
-    PromptSetGroup(PromptKey2, PromptGroup)
-    Citizen.InvokeNative(0xC5F428EE08FA7F2C, PromptKey2, true)
-    PromptRegisterEnd(PromptKey2)
+    str = TEXTS.Prompt2
+    PromptKey2 = UiPromptRegisterBegin()
+    UiPromptSetControlAction(PromptKey2, Config.Prompts.Prompt2)
+    str = VarString(10, 'LITERAL_STRING', str)
+    UiPromptSetText(PromptKey2, str)
+    UiPromptSetEnabled(PromptKey2, true)
+    UiPromptSetVisible(PromptKey2, true)
+    UiPromptSetStandardMode(PromptKey2, true)
+    UiPromptSetGroup(PromptKey2, PromptGroup, 0)
+    UiPromptRegisterEnd(PromptKey2)
     prompts[#prompts + 1] = PromptKey2
 end
 
-Citizen.CreateThread(function()
+CreateThread(function()
+    repeat Wait(5000) until LocalPlayer.state.IsInSession
     LoadPrompts()
+
     while true do
-        Citizen.Wait(500)
-        pcoords = GetEntityCoords(PlayerPedId())
-        isdead = IsEntityDead(PlayerPedId())
-    end
-end)
+        local sleep = 1000
+        local pped = PlayerPedId()
+        local pcoords = GetEntityCoords(pped)
+        local isdead = IsEntityDead(pped)
 
-
-
-Citizen.CreateThread(function()
-    while true do
-        local t = 5
-        if pcoords and (isdead ~= nil and isdead == false) then
-            for i, v in pairs(Config.Graves) do
+        if not isdead then
+            for index, v in ipairs(Config.Graves) do
                 local dist = #(pcoords - v.coords)
+                if dist < 3.0 then
+                    repeat
+                        -- optimisie loop theres 250 entries in the config.Graves table
+                        dist = #(pcoords - v.coords)
+                        sleep = 300
+                        if dist < 1.0 then
+                            sleep = 0
+                            local label = VarString(10, 'LITERAL_STRING', TEXTS.GraveDisplay .. " " .. v.name)
+                            UiPromptSetActiveGroupThisFrame(PromptGroup, label, 0, 0, 0, 0)
 
-                if dist < 1.0 then
-                    local label = CreateVarString(10, 'LITERAL_STRING', TEXTS.GraveDisplay .. " " .. v.name)
-                    PromptSetActiveGroupThisFrame(PromptGroup, label)
-                    if Citizen.InvokeNative(0xC92AC953F0A982AE, PromptKey) then
-                        TriggerEvent("ricx_grave_robbery:dig", i)
-                        Citizen.Wait(2000)
-                    end
-                    if Citizen.InvokeNative(0xC92AC953F0A982AE, PromptKey2) then
-                        TriggerEvent("ricx_grave_robbery:pray", i)
-                        Citizen.Wait(2000)
-                    end
+                            if UiPromptHasStandardModeCompleted(PromptKey, 0) then
+                                TriggerEvent("ricx_grave_robbery:dig", index)
+                                Wait(2000)
+                            end
+
+                            if UiPromptHasStandardModeCompleted(PromptKey2, 0) then
+                                TriggerEvent("ricx_grave_robbery:pray", index)
+                                Wait(2000)
+                            end
+                        end
+                        isdead = IsEntityDead(pped)
+                        Wait(sleep)
+                    until dist > 3.0 or isdead
                 end
             end
-        else
-            t = 1500
         end
-        Citizen.Wait(t)
+        Wait(sleep)
     end
 end)
 
@@ -96,27 +113,29 @@ function EndShovel()
     ClearPedTasks(PlayerPedId())
 end
 
-RegisterNetEvent("ricx_grave_robbery:dig")
 AddEventHandler("ricx_grave_robbery:dig", function(id)
     if praying then
         TriggerEvent("Notification:left_grave_robbery", TEXTS.GraveRobbery, TEXTS.CantDoThat, TEXTURES.alert[1],
             TEXTURES.alert[2], 2000)
         return
     end
+
     if digging then
         EndShovel()
     else
+        local pcoords = GetEntityCoords(PlayerPedId())
+        local town = getTown(pcoords) or "Unknown"
         local hour = GetClockHours()
         if Config.UseHours then
             if hour >= Config.Hours[1] or hour < Config.Hours[2] then
-                TriggerServerEvent("ricx_grave_robbery:check_shovel", id, Town)
+                TriggerServerEvent("ricx_grave_robbery:check_shovel", id, town)
             elseif hour >= Config.Hours[2] then
                 TriggerEvent("Notification:left_grave_robbery", TEXTS.GraveRobbery, "Can only be robbed at night",
                     TEXTURES.alert[1],
                     TEXTURES.alert[2], 2000)
             end
         else
-            TriggerServerEvent("ricx_grave_robbery:check_shovel", id, Town)
+            TriggerServerEvent("ricx_grave_robbery:check_shovel", id, town)
         end
     end
 end)
@@ -126,8 +145,7 @@ function AttachEnt(from, to, boneIndex, x, y, z, pitch, roll, yaw, useSoftPinnin
         vertex, fixedRot, false, false)
 end
 
-RegisterNetEvent("ricx_grave_robbery:start_dig")
-AddEventHandler("ricx_grave_robbery:start_dig", function(id)
+RegisterNetEvent("ricx_grave_robbery:start_dig", function(heading)
     if shovelObject then
         DeleteObject(shovelObject)
         SetEntityAsNoLongerNeeded(shovelObject)
@@ -137,34 +155,41 @@ AddEventHandler("ricx_grave_robbery:start_dig", function(id)
     local pedp = PlayerPedId()
     local pc = GetEntityCoords(pedp)
     local model = Config.Dig.shovel
-    RequestModel(model)
-    while not HasModelLoaded(model) do
-        Citizen.Wait(0)
+
+    if not HasModelLoaded(model) then
+        RequestModel(model, false)
+        while not HasModelLoaded(model) do
+            Wait(0)
+        end
     end
+
     shovelObject = CreateObject(model, pc.x, pc.y, pc.z, true, true, true)
     local boneIndex = GetEntityBoneIndexByName(pedp, Config.Dig.bone)
     local Attach = Config.Dig.pos
-    local heading = Config.Graves[id].heading
     SetEntityHeading(PlayerPedId(), heading)
     local anim = Config.Dig.anim
-    RequestAnimDict(anim[1])
-    while not HasAnimDictLoaded(anim[1]) do
-        Citizen.Wait(0)
+
+    if not HasAnimDictLoaded(anim[1]) then
+        RequestAnimDict(anim[1])
+        while not HasAnimDictLoaded(anim[1]) do
+            Wait(0)
+        end
     end
+
     TaskPlayAnim(pedp, anim[1], anim[2], 1.0, 1.0, -1, 1, 0, false, false, false)
     AttachEnt(shovelObject, pedp, boneIndex, Attach[1], Attach[2], Attach[3], Attach[4], Attach[5], Attach[6], 0, 1, 1, 1)
-    TriggerEvent("ricx_grave_robbery:digging_timer", id)
-    Citizen.Wait(200)
+    TriggerEvent("ricx_grave_robbery:digging_timer")
+    Wait(200)
     RemoveAnimDict(anim[1])
     SetModelAsNoLongerNeeded(model)
 end)
 
-RegisterNetEvent("ricx_grave_robbery:digging_timer")
+
 AddEventHandler("ricx_grave_robbery:digging_timer", function(id)
     local timer = Config.DiggingTimer
     local timer2 = 0
     while timer2 ~= timer and digging do
-        Citizen.Wait(1000)
+        Wait(1000)
         timer2 = timer2 + 1
         if not digging then
             break
@@ -176,7 +201,7 @@ AddEventHandler("ricx_grave_robbery:digging_timer", function(id)
     end
 end)
 
-RegisterNetEvent("ricx_grave_robbery:pray")
+
 AddEventHandler("ricx_grave_robbery:pray", function(id)
     if digging then
         TriggerEvent("Notification:left_grave_robbery", TEXTS.GraveRobbery, TEXTS.CantDoThat, TEXTURES.alert[1],
@@ -189,14 +214,15 @@ AddEventHandler("ricx_grave_robbery:pray", function(id)
         local randomAnim = math.random(1, #Config.PrayAnim)
         local anim = Config.PrayAnim[randomAnim]
         local heading = Config.Graves[id].heading
-        RequestAnimDict(anim[1])
-        while not HasAnimDictLoaded(anim[1]) do
-            Citizen.Wait(0)
+        if not HasAnimDictLoaded(anim[1]) then
+            RequestAnimDict(anim[1])
+            while not HasAnimDictLoaded(anim[1]) do
+                Wait(0)
+            end
         end
         SetEntityHeading(PlayerPedId(), heading)
-        TaskPlayAnim(PlayerPedId(), anim[1], anim[2], 1.0, 1.0, -1, 1, 0, true, 0, false, 0, false)
-        Citizen.Wait(500)
-        Citizen.Wait(500)
+        TaskPlayAnim(PlayerPedId(), anim[1], anim[2], 1.0, 1.0, -1, 1, 0, true, 0, false, "", false)
+        Wait(1000)
         RemoveAnimDict(anim[1])
     end
     praying = not praying
@@ -209,8 +235,8 @@ AddEventHandler('onResourceStop', function(resourceName)
     if praying or digging then
         EndShovel()
     end
-    for i, v in pairs(prompts) do
-        PromptDelete(v)
+    for _, v in pairs(prompts) do
+        UiPromptDelete(v)
     end
 end)
 ----------------------------Basic Notification----------------------------
@@ -219,44 +245,3 @@ RegisterNetEvent('Notification:left_grave_robbery')
 AddEventHandler('Notification:left_grave_robbery', function(t1, t2, dict, txtr, timer)
     TriggerEvent("vorp:TipBottom", t1 .. "\n" .. t2, timer)
 end)
-
-
-RegisterNetEvent('vorp:SelectedCharacter', function()
-    local player = GetPlayerServerId(tonumber(PlayerId()))
-    Wait(100)
-    TriggerServerEvent("outsider_robbery:sendPlayers", player)
-end)
-
-Citizen.CreateThread(
-    function()
-        while true do
-            Citizen.Wait(1000)
-
-            local x, y, z = table.unpack(GetEntityCoords(PlayerPedId()))
-            local ZoneTypeId = 1
-            local current_district = Citizen.InvokeNative(0x43AD8FC02B429D33, x, y, z, ZoneTypeId)
-
-            if current_district then
-                if current_district == 459833523 then
-                    Town = "Valentine"
-                elseif current_district == 1053078005 then
-                    Town = "Blackwater"
-                elseif current_district == 427683330 then
-                    Town = "Strawberry"
-                elseif current_district == -765540529 then
-                    Town = "Saint Denis"
-                elseif current_district == 7359335 then
-                    Town = "Annesburg"
-                elseif current_district == -744494798 then
-                    Town = "Armadillo"
-                elseif current_district == 2046780049 then
-                    Town = "Rhodes"
-                elseif current_district == 2126321341 then
-                    Town = "Vanhorn"
-                elseif current_district == -1524959147 then
-                    Town = "Tumbleweed"
-                end
-            end
-        end
-    end
-)
--- server.lua
+++ server.lua
@@ -1,23 +1,38 @@
-
-local data = {}
 local VorpCore = exports.vorp_core:GetCore()
-local VorpInv = exports.vorp_inventory:vorp_inventoryApi()
 local JobsTable = {}
 local TEXTS = Config.Texts
 local TEXTURES = Config.Textures
-local DiggedGraves = {}
 
-RegisterServerEvent("ricx_grave_robbery:check_shovel")
-AddEventHandler("ricx_grave_robbery:check_shovel", function(id, Town)
+local DIGGED_GRAVES <const> = {}
+local DIGGING_GRAVE <const> = {}
+
+RegisterServerEvent("ricx_grave_robbery:check_shovel", function(id, Town)
     local _source = source
 
-    if DiggedGraves[id] == true then
+    if DIGGED_GRAVES[id] == true then
         TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, TEXTS.GraveRobbed,
             TEXTURES.alert[1], TEXTURES.alert[2], 2000)
         return
     end
 
-    local item = VorpInv.getItem(_source, Config.ShovelItem)
+    if DIGGING_GRAVE[_source] then
+        return print("Player is already digging a grave", GetPlayerName(_source), "id:", id,
+            "Possible exploit detected!!")
+    end
+
+    local playerCoords = GetEntityCoords(GetPlayerPed(_source))
+    local graveCoords = Config.Graves[id].coords
+    local distance = #(playerCoords - graveCoords)
+    if distance > 10.0 then
+        return print("Player is too far away from the grave", GetPlayerName(_source), "id:", id,
+            "Possible exploit detected!!")
+    end
+
+    if not Config.Graves[id].ToolRequired then
+        return print("missing tool required for grave", "at index:", id)
+    end
+
+    local item = exports.vorp_inventory:getItem(_source, Config.Graves[id].ToolRequired)
     if item then
         if not next(item.metadata) then
             -- if not metadata we add new values
@@ -26,9 +41,12 @@ AddEventHandler("ricx_grave_robbery:check_shovel", function(id, Town)
                 durability = 100 - 3,
                 id = item.id
             }
-            VorpInv.setItemMetadata(_source, item.id, newData)
-            TriggerClientEvent("ricx_grave_robbery:start_dig", _source, id)
-            TriggerEvent("outsider_alertjobs", _source,Town)
+            exports.vorp_inventory:setItemMetadata(_source, item.id, newData)
+            DIGGING_GRAVE[_source] = id
+            TriggerClientEvent("ricx_grave_robbery:start_dig", _source, Config.Graves[id].heading)
+            SetTimeout(10000, function() -- wait 10 seconds to alert jobs
+                TriggerEvent("outsider_alertjobs", _source, Town)
+            end)
         else
             if item.metadata.durability <= 0 then
                 TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, "Shovel is broken",
@@ -42,55 +60,61 @@ AddEventHandler("ricx_grave_robbery:check_shovel", function(id, Town)
                 id = item.metadata.id
             }
 
-            VorpInv.setItemMetadata(_source, item.metadata.id, newData)
-            TriggerClientEvent("ricx_grave_robbery:start_dig", _source, id)
-            SetTimeout(10000,function()    -- wait 10 seconds
-               TriggerEvent("outsider_alertjobs",_source, Town)
+            exports.vorp_inventory:setItemMetadata(_source, item.metadata.id, newData)
+            TriggerClientEvent("ricx_grave_robbery:start_dig", _source, Config.Graves[id].heading)
+            DIGGING_GRAVE[_source] = id
+            SetTimeout(10000, function() -- wait 10 seconds to alert jobs
+                TriggerEvent("outsider_alertjobs", _source, Town)
             end)
         end
     else
-        TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, TEXTS.NoShovel,  TEXTURES.alert[1], TEXTURES.alert[2], 2000)
+        TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, TEXTS.NoShovel,
+            TEXTURES.alert[1], TEXTURES.alert[2], 2000)
     end
 end)
 
-local Lines = {
-    "You have found nothing the person buried here was poor as hell",
-    "All that hard work for nothing damn fool",
-    "Why not be a farmer cant find shit with your luck",
-    "You thought it was easy? rob somone alive ",
-    "God is watching you and has punished you ,just like he pusnished the man in here your next..."
 
-}
 
-RegisterServerEvent("ricx_grave_robbery:reward")
-AddEventHandler("ricx_grave_robbery:reward", function(id)
+RegisterServerEvent("ricx_grave_robbery:reward", function()
     local _source = source
-    Citizen.Wait(math.random(200, 800))
+    local id <const> = DIGGING_GRAVE[_source]
+    if not id then
+        return print("Possible exploit detected!!", GetPlayerName(_source), " called this event without digging a grave")
+    end
 
-    local Rewards = Config.Graves[id].Rewards
-    local random = math.random(1, #Rewards)
-
-    if DiggedGraves[id] == true then
-        TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, TEXTS.GraveRobbed, TEXTURES.alert[1], TEXTURES.alert[2], 2000)
+    if DIGGED_GRAVES[id] then
+        TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, TEXTS.GraveRobbed,
+            TEXTURES.alert[1], TEXTURES.alert[2], 2000)
         return
     end
+    DIGGED_GRAVES[id] = true
 
-    DiggedGraves[id] = true
-    local lucky = 2
-    local chance = math.random(1, 5)
-    if lucky == chance then
-        local Item = Config.Graves[id].Rewards[random].item
-        local label = Config.Graves[id].Rewards[random].label
-        VorpInv.addItem(_source, Item, 1)
-        TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery,  TEXTS.FoundItem .. "\n+ " .. label  , TEXTURES.alert[1], TEXTURES.alert[2], 2000)
-    else
-        local rand = math.random(1, #Lines)
-        TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, Lines[rand], TEXTURES.alert[1], TEXTURES.alert[2], 2000)
+    local Rewards = Config.Graves[id].Rewards
+    for _, value in pairs(Rewards) do
+        if math.random() <= value.chance then
+            local canCarryItem = exports.vorp_inventory:canCarryItem(_source, value.item, value.amount)
+            if canCarryItem then
+                exports.vorp_inventory:addItem(_source, value.item, value.amount)
+                TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery,
+                    TEXTS.FoundItem .. "\n+ " .. value.label, TEXTURES.alert[1], TEXTURES.alert[2], 2000)
+            end
+        else
+            local rand = math.random(1, #Config.Lines)
+            TriggerClientEvent("Notification:left_grave_robbery", _source, TEXTS.GraveRobbery, Config.Lines[rand],
+                TEXTURES.alert[1], TEXTURES.alert[2], 2000)
+        end
     end
+
+    if Config.GraveRobberyCooldown then
+        SetTimeout(Config.GraveRobberyCooldown * 60000, function()
+            DIGGED_GRAVES[id] = nil
+        end)
+    end
+    DIGGING_GRAVE[_source] = nil
 end)
 
-function CheckTable(table, element)
-    for k, v in pairs(table) do
+local function CheckTable(table, element)
+    for _, v in pairs(table) do
         if v == element then
             return true
         end
@@ -98,16 +122,10 @@ function CheckTable(table, element)
     return false
 end
 
-RegisterServerEvent("outsider_robbery:sendPlayers", function(source)
-    if not source then return end
-    local _source = source
-    local user = VorpCore.getUser(_source)
-
-    if user then
-        local job = user.getUsedCharacter.job                           -- player job
-        if CheckTable(Config.JobsToAlert, job) then                     -- if player exist and job equals to config then add to table
-            JobsTable[#JobsTable + 1] = { source = _source, job = job } -- id
-        end
+AddEventHandler("vorp:SelectedCharacter", function(source, char)
+    local job = char.job
+    if CheckTable(Config.JobsToAlert, job) then
+        JobsTable[#JobsTable + 1] = { source = source, job = job }
     end
 end)
 
@@ -119,28 +137,29 @@ AddEventHandler('playerDropped', function()
             JobsTable[index] = nil
         end
     end
+
+    DIGGING_GRAVE[_source] = nil
 end)
 
 
 AddEventHandler('outsider_alertjobs', function(source, Town)
-
     if Config.outsider_jobalert?.usealert then
         return exports.outsider_jobalerts:InsertAlert(source, Config.outsider_jobalert.command)
     end
 
     for _, jobHolder in pairs(JobsTable) do
-        
         if Config.synSociety and not Config.outsider_policeman then
             local onduty = exports.syn_society:IsPlayerOnDuty(jobHolder.source, jobHolder.job)
             if onduty then
-                VorpCore.NotifyLeft(jobHolder.source, Town, "grave robbery was witnessed ", "generic_textures", "temp_pedshot", 8000, "COLOR_WHITE")
+                VorpCore.NotifyLeft(jobHolder.source, Town, "grave robbery was witnessed ", "generic_textures",
+                    "temp_pedshot", 8000, "COLOR_WHITE")
             end
         elseif Config.outsider_policeman then
             local onduty = exports.outsider_policeman:IsOnPoliceDuty(jobHolder.source)
             if onduty then
-                VorpCore.NotifyLeft(jobHolder.source, Town, "grave robbery was witnessed ", "generic_textures", "temp_pedshot", 8000, "COLOR_WHITE")
+                VorpCore.NotifyLeft(jobHolder.source, Town, "grave robbery was witnessed ", "generic_textures",
+                    "temp_pedshot", 8000, "COLOR_WHITE")
             end
         end
     end
 end)
-
